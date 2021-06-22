262faeeaac686d6a597fe0427c2c8d5268b598a8
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-22 08:29:50 -0400

    Add note form

--- src/note.js
+++ src/note.js
@@ -1,8 +1,28 @@
 class Note {
     static all = [];
+    static notePopup = document.getElementById("note-popup");
 
     static renderForm() {
-        debugger;
+        // debugger;
+        Calendar.calendarContainer.innerHTML += `
+            <form id="note-popup">
+                <h3>Add a Note</h3>
+                <br>
+                <label>Active?</label> 
+                <input type="checkbox" id="active-yes" value="Y">
+                <label for="active-yes">Y</label>
+                <input type="checkbox" id="active-no" value="N">
+                <label for="active-no">N</label>
+                <br>
+                <label>Note</label>
+                <input type="text" id="content">
+                <br>
+                <button id="update-button">Update</button>
+                <button id="cancel-button">Cancel</button>
+            </form>
+        `
+        // this.notePopup.addEventListener("submit", this.handleSubmit);
+        // this.notePopup.addEventListener("click", this.handleClick);
     }
 
     constructor({id, content, is_active, calendar_id, day_id}) {
@@ -14,7 +34,24 @@ class Note {
         this.element = document.createElement("li");
         this.element.dataset.id = id;
         this.element.id = `note-${this.id}`;
-
+        
         Note.all.push(this);
     }
+    
+    // static handleSubmit(event) {
+    //     debugger;
+    //     event.preventDefault(); 
+    //     console.log("Update button pressed!");
+    // }
+
+    // static handleClick(event) {
+    //     debugger;
+    //     if (event.target.innerText === "Update") {
+    //         console.log("update button pressed");
+    //     }
+    //     if (event.target.innerText === "Cancel") {
+    //         console.log("cancel button pressed");
+    //         Note.notePopup.remove();
+    //     }
+    // }
 }
