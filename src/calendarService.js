f0f6c1b35ce9451bdcef6b318b1a2d7941e45a78
Samantha Ostrowski <isphinxs@hotmail.com>
2021-06-27 12:51:27 -0400

    Handle nonsensical calendar start and end dates

--- src/calendarService.js
+++ src/calendarService.js
@@ -4,29 +4,46 @@ class CalendarService {
     }
 
     createCalendar() {
-        const calendar = {
-            title: document.getElementById("title").value,
-            start_month: document.getElementById("start-month").value,
-            start_year: document.getElementById("start-year").value,
-            end_month: document.getElementById("end-month").value,
-            end_year: document.getElementById("end-year").value
+        const startMonth = document.getElementById("start-month").value;
+        const startYear = document.getElementById("start-year").value;
+        const endMonth = document.getElementById("end-month").value;
+        const endYear = document.getElementById("end-year").value;
+
+        const startDate = new Date(startYear, startMonth, 1);
+        const endDate = new Date(endYear, endMonth, 1);
+
+        // debugger; 
+
+        if (endDate < startDate) {
+            // debugger;
+            document.getElementById("new-calendar-form").reset();
+            alert("Check your dates! The end date must equal to, or come after, the start date.")
+        } else {
+            const calendar = {
+                title: document.getElementById("title").value,
+                start_month: startMonth,
+                start_year: startYear,
+                end_month: endMonth,
+                end_year: endYear
+            }
+            
+            const configObject = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(calendar)
+            }
+            
+            fetch(`${this.endpoint}/calendars`, configObject)
+            .then(resp => resp.json())
+            .then(calendar => {
+                const c = new Calendar(calendar);
+                c.addToDom();
+                Calendar.calendarForm.style.display = "none";
+            })
         }
-        
-        const configObject = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(calendar)
-        }
-        
-        fetch(`${this.endpoint}/calendars`, configObject)
-        .then(resp => resp.json())
-        .then(calendar => {
-            const c = new Calendar(calendar);
-            c.addToDom();
-            Calendar.calendarForm.style.display = "none";
-        })
+
     }
     
     updateCalendar(id) {
